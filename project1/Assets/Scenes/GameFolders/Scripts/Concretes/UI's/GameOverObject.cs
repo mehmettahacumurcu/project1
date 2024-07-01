cb81b1ee662f22c50af72cccaebdaff8f0abf4a8
Mehmet Taha Cumurcu <m.tahacumurcu@gmail.com>
2024-07-01 21:08:42 +0300

    Game Over Panel

---
+++ project1/Assets/Scenes/GameFolders/Scripts/Concretes/UI's/GameOverObject.cs
@@ -0,0 +1,45 @@
+using project1.managers;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+namespace project1.UIs
+{
+    public class GameOverObject : MonoBehaviour
+    {
+        [SerializeField] GameObject gameOverPanel;
+        [SerializeField] Button tryAgainButton;
+        [SerializeField] Button exitButton;
+
+        private void Awake()
+        {
+            if (gameOverPanel.activeSelf)
+            {
+                gameOverPanel.SetActive(false);
+            }
+
+            tryAgainButton.gameObject.SetActive(false);
+            exitButton.gameObject.SetActive(false);
+        }
+
+        private void OnEnable()
+        {
+            GameManager.Instance.OnGameOver += HandleOnGameOver;
+        }
+
+        private void HandleOnGameOver()
+        {
+            gameOverPanel.SetActive(true);
+            tryAgainButton.gameObject.SetActive(true);
+            exitButton.gameObject.SetActive(true);
+        }
+
+        private void OnDisable()
+        {
+            GameManager.Instance.OnGameOver -= HandleOnGameOver;
+        }
+    }
+
+}
